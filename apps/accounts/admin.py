6af3feb89f736c0ebb7c0b8537463616cc2d5388
Edvinas Jurevicius <ikonitas@gmail.com>
2018-03-07 14:40:30 +0000

    Add accounts management for organisations

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
 from .forms import UserCreationForm
 from .models import User
@@ -26,3 +28,36 @@ class UserAdmin(BaseUserAdmin):
         ('Organisations', {'fields': ('organisations',)}),
         ('Important dates', {'fields': ('last_login', 'date_joined'), 'classes': ('collapse',)}),
     ]
+
+    def response_change(self, request, obj):
+        """ Add permission to the users if it's set to staff and belongs to the organisation. """
+        response = super().response_change(request, obj)
+        self.add_remove_permissions(obj)
+        return response
+
+    def get_queryset(self, request):
+        """ Only display users which belongs for the requested user. """
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser and request.user.organisations.exists():
+            qs = qs.filter(organisations__in=request.user.organisations.all())
+        return qs
+
+    def add_remove_permissions(self, obj):
+        if obj.is_staff:
+            content_type = ContentType.objects.get_for_model(User)
+            try:
+                permission = Permission.objects.get(
+                    content_type=content_type, codename='change_user'
+                )
+            except Permission.DoesNotExist:
+                pass
+            else:
+                if obj.organisations.exists():
+                    obj.user_permissions.add(permission)
+                else:
+                    permission_string = '{app_label}.{permission}'.format(
+                        app_label=permission.content_type.app_label,
+                        permission=permission.codename,
+                    )
+                    if obj.has_perm(permission_string):
+                        obj.user_permissions.remove(permission)
---
+++ apps/accounts/tests/test_admin.py
@@ -0,0 +1,55 @@
+from django.contrib.admin.sites import AdminSite
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from django.test import TestCase, RequestFactory
+
+from accounts.admin import UserAdmin
+from accounts.models import User
+from directory.tests.factories import OrganisationFactory
+
+from .factories import UserFactory
+
+
+class UserAdminTest(TestCase):
+
+    def setUp(self):
+        self.user_admin = UserAdmin(User, AdminSite())
+        self.superuser = UserFactory.create(is_superuser=True)
+
+        self.organisation = OrganisationFactory.create()
+        self.user_with_org = UserFactory.create(is_staff=True)
+        self.user_with_org.organisations.add(self.organisation)
+
+        self.user_with_org_1 = UserFactory.create(is_staff=True)
+        self.user_with_org_1.organisations.add(self.organisation)
+
+    def test_permissions_with_no_organisation(self):
+        self.user_admin.add_remove_permissions(self.superuser)
+        self.assertFalse(self.superuser.user_permissions.exists())
+
+    def test_permissions_with_organisation(self):
+        self.user_admin.add_remove_permissions(self.user_with_org)
+        content_type = ContentType.objects.get_for_model(User)
+        permission = Permission.objects.get(
+            content_type=content_type, codename='change_user'
+        )
+        permission_string = '{app_label}.{permission}'.format(
+            app_label=permission.content_type.app_label,
+            permission=permission.codename,
+        )
+
+        self.assertTrue(self.user_with_org.user_permissions.exists())
+        self.assertTrue(self.user_with_org.has_perm(permission_string))
+
+    def test_permissions_delete_organisation(self):
+        self.user_with_org.organisations.remove(self.organisation)
+        self.user_admin.add_remove_permissions(self.user_with_org)
+
+        self.assertFalse(self.user_with_org.user_permissions.exists())
+
+    def test_get_queryset(self):
+        request = RequestFactory().request()
+        request.user = self.user_with_org
+        qs = self.user_admin.get_queryset(request)
+
+        self.assertEqual(qs.count(), 2)
