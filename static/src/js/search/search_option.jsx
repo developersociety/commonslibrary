2c8d057ab0ada422e45e5bdb6d9912aa2b45c6fc
richpeters <richyp@bu.edu>
2018-03-08 13:16:04 +0000

    Add plus and cross icons to sprite - add to tags

--- static/src/js/search/search_option.jsx
+++ static/src/js/search/search_option.jsx
@@ -13,11 +13,18 @@ export class SearchOption extends React.Component {
 
   render() {
     let activeClass = this.props.active ? 'tag active' : 'tag';
+    let optionIcon = (<svg className="icon"><use xlinkHref="#add"></use></svg>);
+
+    if (this.props.active) {
+      optionIcon = (<svg className="icon"><use xlinkHref="#remove"></use></svg>);
+    }
+
     return (
       <div
         className={activeClass}
         onClick={this.optionClick}>
         {this.props.option.name}
+        {optionIcon}
       </div>
     )
   }
