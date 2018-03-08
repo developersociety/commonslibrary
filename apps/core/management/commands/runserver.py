9fdd2de7216f89a5fe96ad2704a3a8ebb50e3ba4
richpeters <richyp@bu.edu>
2018-03-08 15:27:53 +0000

    New webpack runserver

--- apps/core/management/commands/runserver.py
+++ apps/core/management/commands/runserver.py
@@ -10,29 +10,56 @@ from django.contrib.staticfiles.management.commands.runserver import (
 
 class Command(StaticfilesRunserverCommand):
 
-    def inner_run(self, *args, **options):
-        os.environ['WEBPACK_PORT'] = '3000'
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
 
-        self.start_webpack()
+        parser.add_argument(
+            '--nowebpack',
+            action='store_false',
+            dest='use_webpack',
+            default=True,
+            help='Tells Django to NOT start webpack.'
+        )
 
-        return super().inner_run(*args, **options)
+    def run(self, **options):
+        if options.get('use_webpack'):
+            self.set_webpack_environment_variables()
+            self.start_webpack(**options)
+
+        super().run(**options)
+
+    def set_webpack_environment_variables(self):
+        # Only set environment variables on the outer process
+        outer_process = 'RUN_MAIN' not in os.environ
+
+        if outer_process:
+            os.environ['DJANGO_IP'] = self.addr
+            os.environ['DJANGO_PORT'] = self.port
+
+            # Now move Django to another port
+            os.environ['WEBPACK_PORT'] = str(int(self.port) + 1)
 
     def start_webpack(self, **options):
+        inner_process = 'RUN_MAIN' in os.environ
+        use_reloader = options.get('use_reloader')
+
+        # Don't start webpack on the inner process with autoreload
+        if inner_process and use_reloader:
+            return
+
         self.stdout.write('>>> Starting webpack')
         self.webpack_process = subprocess.Popen(
-            ['npm run serve'],
-            shell=True,
+            ['node', 'server.js'],
+            shell=False,
             stdin=subprocess.PIPE,
             stdout=self.stdout,
             stderr=self.stderr,
         )
+        self.stdout.write('>>> webpack process on pid {}'.format(self.webpack_process.pid))
 
-        self.stdout.write(
-            '>>> webpack-dev-server process on pid {0}'.format(self.webpack_process.pid)
-        )
-
-        def kill_webpack_process(pid):
+        def kill_webpack_process():
             self.stdout.write('>>> Closing webpack process')
-            os.kill(pid, signal.SIGTERM)
+            self.webpack_process.terminate()
+            self.webpack_process.wait()
 
-        atexit.register(kill_webpack_process, self.webpack_process.pid)
+        atexit.register(kill_webpack_process)
