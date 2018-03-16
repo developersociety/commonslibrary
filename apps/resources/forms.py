79c68f50c5c10cdf89fe25d4a9c4d7134526705a
Alex Tomkins <tomkins@darkzone.net>
2018-03-16 18:17:31 +0000

    Fix resource creation - add created_by/updated_by

--- apps/resources/forms.py
+++ apps/resources/forms.py
@@ -13,9 +13,14 @@ class ResourceForm(forms.ModelForm):
         fields = ('title', 'abstract', 'content', 'tags', 'image', 'organisation', 'privacy')
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user')
+        self.user = kwargs.pop('user')
         super().__init__(*args, **kwargs)
-        self.fields['organisation'].queryset = user.approved_organisations.all()
-        self.fields['privacy'].queryset = user.approved_organisations.all()
+        self.fields['organisation'].queryset = self.user.approved_organisations.all()
+        self.fields['privacy'].queryset = self.user.approved_organisations.all()
         self.helper = FormHelper()
         self.helper.add_input(Submit('submit', 'Submit'))
+
+    def save(self, commit=True):
+        self.instance.created_by = self.user
+        self.instance.updated_by = self.user
+        return super().save(commit=commit)
