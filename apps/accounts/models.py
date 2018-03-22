7cad8cdf34cc815893a6d1a5b83e09390fde6280
richpeters <richyp@bu.edu>
2018-03-22 16:15:47 +0000

    Add links to directory pages

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.postgres.fields import CIEmailField
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 from sorl.thumbnail import ImageField
