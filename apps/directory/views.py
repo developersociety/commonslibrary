c05ee1c17f39daf82c0bc068145b934ed7a2b3ce
richpeters <richyp@bu.edu>
2018-03-23 11:11:23 +0000

    Add user detail view by PK

--- apps/directory/views.py
+++ apps/directory/views.py
@@ -2,6 +2,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import Organisation
+from accounts.models import User
 
 
 class OrganisationListView(ListView):
@@ -13,4 +14,15 @@ class OrganisationDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        return context
+
+class OrganisationUserView(DetailView):
+    model = User
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = context['user']
+        context['resources_created'] = user.resources_created.approved().count()
+        context['resources_liked'] = user.resources_likes.approved().count()
+        context['resources_tried'] = user.resources_tried.approved().count()
         return context
