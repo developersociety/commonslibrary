7e1c59042ca5120319b9b86e7436dd8b9065ef7e
Edvinas Jurevicius <ikonitas@gmail.com>
2018-03-26 14:02:28 +0100

    Sort imports

--- apps/api/v1/tests/test_views.py
+++ apps/api/v1/tests/test_views.py
@@ -1,8 +1,7 @@
 from django.urls import reverse
 
 from rest_framework import status
-from rest_framework.test import APIClient
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient, APITestCase
 
 from accounts.tests.factories import UserFactory
 from directory.tests.factories import OrganisationFactory
