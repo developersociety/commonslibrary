99ea988a65975278dd55f030add141a985dfd1c9
Edvinas Jurevicius <ikonitas@gmail.com>
2018-03-27 14:48:12 +0100

    Add test to make sure it's returns tag list

--- apps/api/v1/tests/test_views.py
+++ apps/api/v1/tests/test_views.py
@@ -6,6 +6,7 @@ from rest_framework.test import APIClient, APITestCase
 from accounts.tests.factories import UserFactory
 from directory.tests.factories import OrganisationFactory
 from resources.tests.factories import ResourceFactory
+from tags.tests.factories import TagFactory
 
 
 class ResourceTests(APITestCase):
@@ -40,5 +41,16 @@ class OrganisationTests(APITestCase):
 
     def test_get_list(self):
         url = reverse('organisation-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TagTests(APITestCase):
+
+    def setUp(self):
+        TagFactory.create()
+
+    def test_get_list(self):
+        url = reverse('tag-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
