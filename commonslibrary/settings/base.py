1abd8115d99644860b07e1802d219158a9cc8eb2
Edvinas Jurevicius <ikonitas@gmail.com>
2018-04-03 10:19:27 +0100

    Change rest framework settings authentication type

--- commonslibrary/settings/base.py
+++ commonslibrary/settings/base.py
@@ -294,11 +294,13 @@ CKEDITOR_CONFIGS = {
 # yapf: enable
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
