53f1da9ddc6f78f2912b40ba0dc93b2f0ba8a975
Edvinas Jurevicius <ikonitas@gmail.com>
2018-04-04 14:32:30 +0100

    Fix mint lint

--- apps/comments/admin.py
+++ apps/comments/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.utils.html import format_html
 from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import Comment, Report
 
@@ -21,19 +21,17 @@ class CommentAdmin(admin.ModelAdmin):
 class ReportAdmin(admin.ModelAdmin):
     list_display = ('id', 'reviewed', 'comment', 'created_by', 'created_at',)
     list_filter = ('reviewed',)
-    fieldsets = (
-        ('Report', {
-            'fields': (
-                'body', 'get_abused_comment', 'get_comment_url', 'get_resource_url', 'reviewed',
-            )
-        }),
+    fieldsets = [
         (
-            'Meta', {
-                'classes': ('collapse',),
-                'fields': ('id', 'created_by', 'created_at',)
+            'Report', {
+                'fields': (
+                    'body', 'get_abused_comment', 'get_comment_url', 'get_resource_url',
+                    'reviewed',
+                )
             }
         ),
-    )
+        ('Meta', {'classes': ('collapse',), 'fields': ('id', 'created_by', 'created_at',)}),
+    ]
 
     def has_add_permission(self, request):
         return False
@@ -44,24 +42,33 @@ class ReportAdmin(admin.ModelAdmin):
         return fields
 
     def get_resource_url(self, obj):
-        return format_html('<a href="{url}" target="_blank">Resource URL</a>'.format(
-            url=obj.comment.resource.get_absolute_url(),
-        ))
+        return format_html(
+            '<a href="{url}" target="_blank">Resource URL</a>'.format(
+                url=obj.comment.resource.get_absolute_url(),
+            )
+        )
+
     get_resource_url.short_description = 'Resource URL'
 
     def get_comment_url(self, obj):
-        return format_html('<a href="{url}" target="_blank">Comment URL</a>'.format(
-            url=reverse('admin:comments_comment_change', args=(obj.comment.id,)),
-        ))
+        return format_html(
+            '<a href="{url}" target="_blank">Comment URL</a>'.format(
+                url=reverse('admin:comments_comment_change', args=(obj.comment.id,)),
+            )
+        )
+
     get_comment_url.short_description = 'Comment URL'
 
     def get_abused_comment(self, obj):
         return obj.comment.body
+
     get_abused_comment.short_description = 'Abused comment text'
 
     def get_queryset(self, request):
         """ Only display resources which belongs for the requested user. """
         qs = super().get_queryset(request)
         if not request.user.is_superuser and request.user.approved_organisations.exists():
-            qs = qs.filter(comment__resource__organisation__in=request.user.approved_organisations.all()).distinct()
+            qs = qs.filter(
+                comment__resource__organisation__in=request.user.approved_organisations.all()
+            ).distinct()
         return qs
