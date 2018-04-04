d2fe8c7416d851859afe31adf18902631fa9e717
richpeters <richyp@bu.edu>
2018-04-04 10:43:33 +0100

    Add context to home page for fetaured resources sections

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 
+from directory.models import Organisation
 from resources.models import Resource
 
 from .mixins import ResourcesViewMixin
@@ -11,6 +12,10 @@ class HomeView(TemplateView, ResourcesViewMixin):
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         context['carousel_resources'] = Resource.get_carousel_resources(self.request.user)
+        context['latest_resource'] = Resource.get_latest(self.request.user)
+        context['most_tried'] = Resource.get_most_tried(self.request.user)
+        context['most_liked'] = Resource.get_most_liked(self.request.user)
+        context['most_published'] = Organisation.get_most_published_this_week()
         return context
 
 
