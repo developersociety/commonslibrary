6e61c1385810834012c78eb5e91a0c7e1133faa0
richpeters <richyp@bu.edu>
2018-04-05 16:33:28 +0100

    Add taggle to tag selection

---
+++ static/src/js/forms/forms.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const file_fields = [...document.querySelectorAll('.file-group')];
+
+class FileUploader extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentFile: null,
+      toClear: false
+    }
+
+    this.handleUpload = this.handleUpload.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.getFileStats = this.getFileStats.bind(this);
+  }
+
+  handleUpload(uploadEvent) {
+    const file = uploadEvent.target.files[0];
+
+    this.setState({
+      currentFile: file
+    })
+  }
+
+  handleClear() {
+    this.setState(prevState => {
+      toClear: !prevState.toClear
+    })
+  }
+
+  getFileStats() {
+    if (this.state.currentFile !== null) {
+      return (
+        <div className="file-uploader__stats">
+          <span className="file-name">{this.state.currentFile.name}, </span>
+          <span className="file-size">{Math.floor(this.state.currentFile.size/1024)}kb</span>
+        </div>
+      )
+    } else if (this.props.inputFile !== null) {
+      return (
+        <div className="file-uploader__stats">
+          <span className="file-name">{this.props.inputFile.textContent}, </span>
+        </div>
+      )
+    } else {
+      return (
+        <div className="file-uploader__stats">
+          <span className="file-name">No file selected.</span>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    // remove initial DOM node
+    this.props.input.remove();
+
+    const fileStats = this.getFileStats();
+    let fileClear = '';
+
+    if (this.props.inputClear) {
+      fileClear = <label for={this.props.inputClear.is} onChange={this.handleClear}></label>
+    }
+
+    return(
+      <div>
+        <input
+          type="file"
+          className="sr__input"
+          name={this.props.input.name}
+          id={this.props.input.id}
+          onChange={this.handleUpload}/>
+        <div className="file-uploader">
+          <label htmlFor={this.props.input.id} className="button input">
+            Pick a file
+          </label>
+          {fileStats}
+        </div>
+        {fileClear}
+      </div>
+    )
+  }
+}
+
+file_fields.map(field => {
+  const mountPoint = field.querySelector('.file-mount');
+  const input = field.querySelector('input[type=file]');
+  const inputClear = field.querySelector('input[type=checkbox]');
+  const inputFile = field.querySelector('a');
+
+  ReactDOM.render(
+    <FileUploader
+      input={input}
+      inputFile={inputFile}
+      inputClear={inputClear}
+      />,
+    mountPoint
+  )
+});
+
