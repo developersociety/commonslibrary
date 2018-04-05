465cd8b0e316a830d46687a16c0ae3e7e9a8f7d0
Edvinas Jurevicius <ikonitas@gmail.com>
2018-04-05 11:48:45 +0100

    Remove unused import

--- apps/api/v1/views.py
+++ apps/api/v1/views.py
@@ -1,5 +1,3 @@
-from distutils.util import strtobool
-
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import detail_route
