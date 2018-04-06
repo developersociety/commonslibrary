f1cbc620a4d99ba487c980719d30ab32ca8b9527
Edvinas Jurevicius <ikonitas@gmail.com>
2018-04-06 12:59:35 +0100

    Add resources statuses

--- apps/resources/tests/test_models.py
+++ apps/resources/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from accounts.tests.factories import UserFactory
+from resources.choices import RESOURCE_APPROVED
 from resources.models import Resource
 from resources.tests.factories import ResourceFactory
 from tags.tests.factories import TagFactory
@@ -10,22 +11,27 @@ class ResourceModelTestCase(TestCase):
 
     def test_get_carousel_resources(self):
         ResourceFactory.create(
-            title='first', likes=UserFactory.create_batch(size=10), is_approved=True
+            title='first', likes=UserFactory.create_batch(size=10), status=RESOURCE_APPROVED
         )
         second = ResourceFactory.create(
-            title='second', tried=UserFactory.create_batch(size=5), hits=20, is_approved=True
+            title='second',
+            tried=UserFactory.create_batch(size=5),
+            hits=20,
+            status=RESOURCE_APPROVED,
         )
         resources = Resource.get_carousel_resources(limit=1)
         self.assertIn(second, resources)
 
     def test_get_related_resources(self):
-        resource = ResourceFactory.create(is_approved=True, tags=TagFactory.create_batch(10))
+        resource = ResourceFactory.create(
+            status=RESOURCE_APPROVED, tags=TagFactory.create_batch(10)
+        )
         tag = resource.tags.all()[0]
         tag_1 = resource.tags.all()[1]
         tag_2 = resource.tags.all()[2]
-        resource_2 = ResourceFactory.create(is_approved=True, tags=[tag])
-        resource_3 = ResourceFactory.create(is_approved=True, tags=[tag_1, tag_2])
-        resource_4 = ResourceFactory.create(is_approved=True, tags=[tag, tag_1, tag_2])
+        resource_2 = ResourceFactory.create(status=RESOURCE_APPROVED, tags=[tag])
+        resource_3 = ResourceFactory.create(status=RESOURCE_APPROVED, tags=[tag_1, tag_2])
+        resource_4 = ResourceFactory.create(status=RESOURCE_APPROVED, tags=[tag, tag_1, tag_2])
         resources = resource.get_related()
         self.assertEqual(resource_4.id, resources[0].id)
         self.assertEqual(resource_3.id, resources[1].id)
