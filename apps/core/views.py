5b27186b057dc1e456f9caa083a0b74c4087ef37
Edvinas Jurevicius <ikonitas@gmail.com>
2018-04-06 10:52:34 +0100

    Elimate dups for most tried/likes

--- apps/core/views.py
+++ apps/core/views.py
@@ -16,8 +16,12 @@ class HomeView(TemplateView, ResourcesViewMixin):
         context = super().get_context_data()
         context['carousel_resources'] = Resource.get_carousel_resources(self.request.user)
         context['latest_resource'] = Resource.get_latest(self.request.user)
-        context['most_tried'] = Resource.get_most_tried(self.request.user)
-        context['most_liked'] = Resource.get_most_liked(self.request.user)
+        most_tried_resource = Resource.get_most_tried(self.request.user).first()
+        context['most_tried'] = most_tried_resource
+        kwargs = {'user': self.request.user}
+        if most_tried_resource:
+            kwargs.update({'exclude': most_tried_resource.id})
+        context['most_liked'] = Resource.get_most_liked(**kwargs).first()
         context['most_published'] = Organisation.get_most_published_this_week()
         return context
 
