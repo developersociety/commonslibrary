5b524bc8ad8ce7a16ad53954b5ac28248c2c8eac
Edvinas Jurevicius <ikonitas@gmail.com>
2018-04-06 10:56:40 +0100

    Address PR comments

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
+from django.core.exceptions import PermissionDenied
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 
@@ -18,7 +18,7 @@ class ReportCommentView(LoginRequiredMixin, DetailView, CreateView):
     def get_object(self, queryset=None):
         obj = super().get_object(queryset=queryset)
         if obj.created_by == self.request.user:
-            raise Http404(
+            raise PermissionDenied(
                 'No {verbose_name}s found matching the query'.format(
                     verbose_name=self.model._meta.verbose_name,
                 )
@@ -70,7 +70,7 @@ class CommentUpdateView(LoginRequiredMixin, UpdateView):
     def get_object(self, queryset=None):
         obj = super().get_object(queryset=queryset)
         if obj.created_by != self.request.user:
-            raise Http404(
+            raise PermissionDenied(
                 'No {verbose_name}s found matching the query'.format(
                     verbose_name=self.model._meta.verbose_name,
                 )
