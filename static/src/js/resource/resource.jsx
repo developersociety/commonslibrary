d3f70b601cc305a9cc084b9e457cb79e90f6cba8
richpeters <richyp@bu.edu>
2018-04-06 10:15:09 +0100

    Add resource detail qa

--- static/src/js/resource/resource.jsx
+++ static/src/js/resource/resource.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { ResourceAction } from './resource_action';
-
 export class Resource extends React.Component {
   render() {
     let resourceClass = 'resource';
@@ -23,33 +21,71 @@ export class Resource extends React.Component {
     }
 
     return (
-      <div className={resourceClass}>
-        {resourceHeader}
-        <div className="resource-summary">
-            <a href={this.props.resource.url}>{this.props.resource.title}</a>
-        </div>
-        <footer className="resource-footer">
-          <div className="resource-user">
-            <div
-                className="resource-user__group"
-                style={resourceOrgLogo}>
-            </div>
-            <p className="resource-user__name">
-              <a href={this.props.resource.created_by_link}>
-                {this.props.resource.created_by}
-              </a>
-            </p>
+      <a href={this.props.resource.url} className={resourceClass}>
+        <div className="resource-blurred">
+          {resourceHeader}
+          <div className="resource-summary">
+            {this.props.resource.title}
           </div>
-          <ResourceAction
-            resource={this.props.resource.id}
-            likes={this.props.resource.likes_count}
-            tries={this.props.resource.tried_count}
-            userLiked={this.props.resource.user_liked}
-            userTried={this.props.resource.user_tried}
-            csrf={this.props.csrf}
-          />
-        </footer>
-      </div>
+          <footer className="resource-footer">
+            <div className="resource-user">
+              <div
+                  className="resource-user__group"
+                  style={{resourceOrgLogo}}>
+              </div>
+              <p className="resource-user__name">
+                {this.props.resource.created_by}
+              </p>
+            </div>
+            <div className="resource-actions">
+              <span
+                className={'resource-liked ' + (this.props.resource.user_liked)}>
+                <svg className="icon">
+                  <use xlinkHref="#liked"></use>
+                </svg>
+                {this.props.resource.likes_count}
+              </span>
+              <span
+                className={'resource-tried ' + (this.props.resource.user_tried)}>
+                <svg className="icon">
+                  <use xlinkHref="#tried"></use>
+                </svg>
+                {this.props.resource.tried_count}
+              </span>
+            </div>
+          </footer>
+        </div>
+        <div className="resource-focused">
+          <div className="resource-list-abstract">
+            {this.props.resource.abstract}
+          </div>
+          <div className="resource-meta">
+            <div className="icon-list">
+              <div className="icon-list__row">
+                <svg className="icon">
+                  <use xlinkHref="#date"></use>
+                </svg>
+                <div className="icon-list__content">
+                  <p>{this.props.resource.created_at}</p>
+                </div>
+              </div>
+
+              {this.props.resource.tags &&
+                <div className="icon-list__row">
+                  <svg className="icon">
+                    <use xlinkHref="#tag"></use>
+                  </svg>
+                  <div className="icon-list__content">
+                    {this.props.resource.tags.map((tag, index) => {
+                      return index != (this.props.resource.tags.length - 1) ? tag.title + ', ' : tag.title
+                    })}
+                  </div>
+                </div>
+              }
+            </div>
+          </div>
+        </div>
+      </a>
     );
   }
 }
