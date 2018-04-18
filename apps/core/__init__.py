371369fa655115fb8272004e904db67b364c304f
Edvinas Jurevicius <ikonitas@gmail.com>
2018-04-18 16:42:59 +0100

    Add monkey path to fix issue with truncated images

--- apps/core/__init__.py
+++ apps/core/__init__.py
@@ -1,6 +1,10 @@
 from django.forms.fields import CharField
 
+from PIL import ImageFile
+
 from .validators import ProhibitNullCharactersValidator
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 # Make CharField form field prohibit null characters
 CharField.default_validators.append(ProhibitNullCharactersValidator())
