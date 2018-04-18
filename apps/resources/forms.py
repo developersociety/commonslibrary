996ef50db4c290ce6342c823abb0d1c572bc732b
Edvinas Jurevicius <ikonitas@gmail.com>
2018-04-18 15:00:41 +0100

    Add extra flat is_public on the add resource form

--- apps/resources/forms.py
+++ apps/resources/forms.py
@@ -8,6 +8,9 @@ from .models import Resource
 
 
 class ResourceForm(forms.ModelForm):
+    is_public = forms.BooleanField(
+        label='Make this resource public', initial=True
+    )
 
     class Meta:
         model = Resource
@@ -22,6 +25,8 @@ class ResourceForm(forms.ModelForm):
         self.fields['organisation'].empty_label = 'Select'
         self.fields['privacy'].queryset = self.user.approved_organisations.all()
         self.fields['privacy'].widget = forms.CheckboxSelectMultiple()
+        self.fields['privacy'].initial = [privacy[0]for privacy in self.fields['privacy'].choices]
+
         self.helper = FormHelper()
         self.helper.layout = Layout(
             'title',
@@ -38,6 +43,7 @@ class ResourceForm(forms.ModelForm):
                 css_class='file-group'
             ),
             'organisation',
+            'is_public',
             'privacy',
             ButtonHolder(
                 Submit('submit', 'Submit your resource', css_class='submit'),
@@ -49,4 +55,8 @@ class ResourceForm(forms.ModelForm):
         self.instance.created_by = self.user
         self.instance.updated_by = self.user
         self.instance.slug = slugify(self.instance.title)
+
+        # If user selected make resource public make sure we emptying the privacy choices.
+        if self.cleaned_data['is_public']:
+            self.cleaned_data['privacy'] = ''
         return super().save(commit=commit)
