5a5a80e9d081f8235bda684132a5a90ec7eed1f4
richpeters <richyp@bu.edu>
2018-05-11 09:53:05 +0100

    Only use carousel if there is more than one slide

--- static/src/js/home.js
+++ static/src/js/home.js
@@ -3,10 +3,12 @@ import Flickity from 'flickity';
 const homeCarousel = document.querySelector('.home-intro');
 const homeSlideCount = homeCarousel.childElementCount
 
-const flkty = new Flickity(homeCarousel, {
-  cellAlign: 'left',
-  pageDots: false,
-  autoPlay: 6000,
-  wrapAround: homeSlideCount > 2 ? true : false,
-  contain: homeSlideCount < 3 ? true : false
-});
+if (homeSlideCount > 1) {
+    const flkty = new Flickity(homeCarousel, {
+      cellAlign: 'left',
+      pageDots: false,
+      autoPlay: 6000,
+      wrapAround: homeSlideCount > 2 ? true : false,
+      contain: homeSlideCount < 3 ? true : false
+    });
+}
