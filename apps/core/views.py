6f87eed51e3f7683223dbe4a56b2ec378106a430
richpeters <richyp@bu.edu>
2018-07-10 13:56:31 +0100

    Remove groups and people from explore view

--- apps/core/views.py
+++ apps/core/views.py
@@ -35,8 +35,6 @@ class ExploreView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['people'] = User.objects.all().order_by('first_name')
-        context['groups'] = Organisation.objects.all().order_by('title')
         context['tags'] = Tag.objects.all()
         context['pages'] = Page.objects.filter(is_active=True)
         return context
