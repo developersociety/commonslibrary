53ea83ed86ab2b01ab8501c77c4dd2345488c45c
richpeters <richard@dev.ngo>
2019-10-02 15:15:08 +0100

    Update styles and Javascript

--- static/src/js/categories.js
+++ static/src/js/categories.js
@@ -1,14 +1,19 @@
 const category_toggles = document.querySelectorAll('.category_toggle');
+const category_details = document.querySelectorAll('.resources-holder');
 
 category_toggles.forEach((category) => {
     category.onclick = (event) => {
         event.preventDefault();
+
+        category_details.forEach((detail_panel) => {
+            detail_panel.classList.add('hidden');
+        });
 
         const target = category.dataset.target;
         const target_element = document.querySelector(
             `.resources-holder[data-category="${target}"]`
         );
 
-        target_element.classList.toggle('hidden');
+        target_element.classList.remove('hidden');
     };
 });
