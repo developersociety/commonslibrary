f64a3db38998a04a5bed003f37b35fc6818a4fe4
richpeters <richard@dev.ngo>
2019-10-02 16:46:29 +0100

    Add extra javascript

--- static/src/js/categories.js
+++ static/src/js/categories.js
@@ -5,15 +5,26 @@ category_toggles.forEach((category) => {
     category.onclick = (event) => {
         event.preventDefault();
 
+        const category_element = category.closest('.category-grid__item');
+        const height_offset = category_element.offsetTop + category_element.offsetHeight;
+
         category_details.forEach((detail_panel) => {
             detail_panel.classList.add('hidden');
         });
+
+        category_toggles.forEach((category_toggle) => {
+            category_toggle.closest('.category-grid__item').classList.remove('active');
+        });
+
+        // Add active to the category
+        category_element.closest('.category-grid__item').classList.add('active');
 
         const target = category.dataset.target;
         const target_element = document.querySelector(
             `.resources-holder[data-category="${target}"]`
         );
 
+        target_element.style.top = `${height_offset}px`;
         target_element.classList.remove('hidden');
     };
 });
