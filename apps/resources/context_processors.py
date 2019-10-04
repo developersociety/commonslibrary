75643a261211d892f9befdb69bd94cfb381c4255
Richard <richard@dev.ngo>
2019-10-04 15:16:15 +0100

    Lint

--- apps/resources/context_processors.py
+++ apps/resources/context_processors.py
@@ -1,9 +1,8 @@
 from .models import ResourceCategory
 
+
 def resource_categories(request):
     """
     Add categories for explore drop down
     """
-    return {
-        'resources_categories': ResourceCategory.objects.all()
-    }
+    return {'resources_categories': ResourceCategory.objects.all()}
