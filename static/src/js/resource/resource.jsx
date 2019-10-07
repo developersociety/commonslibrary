0e01047db49eb1adb53cf7615a7fe202882e0394
Lucy <lucy@dev.ngo>
2019-10-07 14:40:46 +0100

    Change to rollover detail

--- static/src/js/resource/resource.jsx
+++ static/src/js/resource/resource.jsx
@@ -64,36 +64,28 @@ export class Resource extends React.Component {
                     </footer>
                 </div>
                 <div className="resource-focused">
-                    <div className="resource-list-abstract">
-                        <p className="resource-abstract">{this.props.resource.abstract}</p>
-                    </div>
-                    <div className="resource-meta">
-                        <div className="icon-list">
-                            <div className="icon-list__row">
-                                <svg className="icon">
-                                    <use xlinkHref="#date" />
-                                </svg>
-                                <div className="icon-list__content">
-                                    <p>{this.props.resource.created_at}</p>
-                                </div>
-                            </div>
-
-                            {this.props.resource.tags.length > 0 && (
-                                <div className="icon-list__row">
-                                    <svg className="icon">
-                                        <use xlinkHref="#tag" />
-                                    </svg>
-                                    <div className="icon-list__content">
-                                        {this.props.resource.tags.map((tag, index) =>
-                                            index != this.props.resource.tags.length - 1
-                                                ? tag.title + ', '
-                                                : tag.title
-                                        )}
-                                    </div>
-                                </div>
-                            )}
+                    {resourceHeader}
+                    <div className="resource-summary">{this.props.resource.title}</div>
+                    <footer className="resource-footer">
+                        <div className="resource-user">
+                            {resourceOrgLogo}
+                            <p className="resource-user__name">{this.props.resource.created_by}</p>
                         </div>
-                    </div>
+                        <div className="resource-actions">
+                            <span className={`resource-liked ${this.props.resource.user_liked}`}>
+                                <svg className="icon">
+                                    <use xlinkHref="#liked" />
+                                </svg>
+                                {this.props.resource.likes_count}
+                            </span>
+                            <span className={`resource-tried ${this.props.resource.user_tried}`}>
+                                <svg className="icon">
+                                    <use xlinkHref="#tried" />
+                                </svg>
+                                {this.props.resource.tried_count}
+                            </span>
+                        </div>
+                    </footer>
                 </div>
             </a>
         );
