5bdc64252ed48226521db95629234ceed06da7e5
Michelle Greensted <71762549+MichelleGreensted@users.noreply.github.com>
2023-06-23 14:53:26 +0100

    Updated resources pagination to show 10 resources per page

--- apps/api/v1/paginations.py
+++ apps/api/v1/paginations.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class ResourcesPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
